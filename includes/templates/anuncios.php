d28bfbcb24f70cb797480d6a1844d3ea520068ba
Steeven Fierro <juansteeven2900@gmail.com>
2025-06-10 19:41:28 -0500

    Nuevos cambios 10/06/2025

--- includes/templates/anuncios.php
+++ includes/templates/anuncios.php
@@ -14,30 +14,31 @@
 <div class="contenedor-anuncios">
   <?php while($propiedad = mysqli_fetch_assoc($resultado)): ?>
     <div class="anuncio">
-      <img loading="lazy" src="/imagenes/<?php echo propiedad['imagen']; ?>" alt="anuncio">
+      <!-- <img loading="lazy" src="build/img/anuncio1.jpg" alt="anuncio"> -->
+      <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="anuncio">
       
 
       <div class="contenido-anuncio">
-        <h3>Casa de Lujo en el Lago</h3>
-        <p>Casa en el lago con excelente vista, acabados de lujo a un excelente precio</p>
-        <p class="precio">$100,000</p>
+        <h3><?php echo $propiedad['titulo']; ?></h3>
+        <p><?php echo $propiedad['descripcion']; ?></p>
+        <p class="precio">$<?php echo $propiedad['precio']; ?></p>
 
         <ul class="iconos-caracteristicas">
           <li>
             <img class="icono" src="build/img/icono_wc.svg" alt="icono wc">
-            <p>3</p>
+            <p><?php echo $propiedad['wc']; ?></p>
           </li>
           <li>
             <img class="icono" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-            <p>3</p>
+            <p><?php echo $propiedad['estacionamiento']; ?></p>
           </li>
           <li>
             <img class="icono" src="build/img/icono_dormitorio.svg" alt="icono dormitorio">
-            <p>4</p>
+            <p><?php echo $propiedad['habitaciones']; ?></p>
           </li>
         </ul>
 
-        <a href="anuncios.php" class="boton-amarillo-block">Ver Propiedad</a>
+        <a href="anuncio.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Ver Propiedad</a>
       </div> <!--.contenido-anuncio-->
     </div><!--.anuncio-->
   <?php endwhile; ?>
@@ -45,4 +46,5 @@
 
 <?php
   // Cerrar la conexión
+  mysqli_close($db);
 ?>
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -30,6 +30,8 @@
                 </div>
                 
             </div> <!--.barra-->
-
+            <?php
+                echo $inicio ? "<h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>" : '';
+            ?>
         </div>
     </header>
